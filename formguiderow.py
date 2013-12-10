9b61e2fda231db93580def68e017b9b578b1384f
Aniket Shah <aniket@anikets.com>
2013-12-10 14:47:51 +0530

    Drafted a new data object - FormGuideRow
    A data model object that holds all the data for a given form guide row

---
+++ formguiderow.py
@@ -0,0 +1,15 @@
+class FormGuideRow:
+    def __init__(self):
+        currentPosition = 0
+        lastPosition = 0
+        # Note that we are going to use the club name displayed
+        # on the form table for now
+        club = ""
+        tableRank = 0
+        homeForm = []
+        homePoints = 0
+        awayForm = []
+        awayPoints = 0
+        lastSixMatches = []
+        totalPoints = 0
+    
