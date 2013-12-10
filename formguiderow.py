df3352ab550f6a28f673847ab5f9d2381a24394f
Aniket Shah <aniket@anikets.com>
2013-12-10 19:04:25 +0530

    Added a method to print FormGuideRow

--- formguiderow.py
+++ formguiderow.py
@@ -12,4 +12,17 @@ class FormGuideRow:
         awayPoints = 0
         lastSixMatches = []
         totalPoints = 0
+
+    def __str__(self):
+        s = "Current Position: " + str(self.currentPosition) + "\n"
+        s = s + "Last Position: " + str(self.lastPosition) + "\n"
+        s = s + "Club: " + self.club + "\n"
+        s = s + "Table Rank: " + str(self.tableRank) + "\n"
+        s = s + "Home Form: " + str(self.homeForm) + "\n"
+        s = s + "Home Points: " + str(self.homePoints) + "\n"
+        s = s + "Away Form: " + str(self.awayForm) + "\n"
+        s = s + "Away Points: " + str(self.awayPoints) + "\n"
+        s = s + "Last Six Matches: " + str(self.lastSixMatches) + "\n"
+        s = s + "Total Points: " + str(self.totalPoints) + "\n"
+        return s
     
