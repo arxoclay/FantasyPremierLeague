d8f641b092a2b5b0476c6e429a50d8d87a24c702
Aniket Shah <aniket@anikets.com>
2013-12-14 08:53:31 +0530

    Added another form based heuristic measure

--- formheuristics.py
+++ formheuristics.py
@@ -5,6 +5,9 @@ from clubnamematcher import ClubNameMatcher
 
 class FormHeuristics:
 
+    hotnessWeights = (50, 30, 10, 5, 3, 2)
+    resultWeights = (100, 60, 0) # W,D,L
+    
     def __init__(self):
         self.fixturesAdapter = FixturesAdapter()
         self.formGuideAdapter = FormGuideAdapter()
@@ -24,6 +27,18 @@ class FormHeuristics:
 
         return formDiscrepancyData
 
+    # Returns the hotness discrepancy corresponding to each fixture row
+    # -> List of (int, FixturesRow)
+    # The weighted form over the last 6 matches is treated as the hotness of a team
+    # The discrepancy will be positive if its in the home team's favor and vice versa
+    def getHotnessDiscrepancy(self):
+        hotnessDiscrepancyData = []
+
+        for fixture in self.fixturesData:
+            hotnessDiscrepancyData.append((self.getHotness(fixture.homeTeam) - self.getHotness(fixture.awayTeam), fixture))
+
+        return hotnessDiscrepancyData
+
     # Returns current position in the form guide data for team
     # -> int
     def getFormRanking(self, team):
@@ -33,9 +48,33 @@ class FormHeuristics:
                 return formGuideRow.currentPosition
         raise NameError("No matching form information found for " + team)
 
+    # Returns "hotness" in the form guide data for team
+    # -> int
+    def getHotness(self, team):
+        clubNameMatcher = ClubNameMatcher()
+        for formGuideRow in self.formGuideData:
+            if (clubNameMatcher.isSameClub(formGuideRow.club, team)):
+                hotness = 0
+                for i in range(len(formGuideRow.lastSixMatches)):
+                    matchResult = formGuideRow.lastSixMatches[i]
+                    if matchResult == "W":
+                        hotness = hotness + (FormHeuristics.resultWeights[0] * FormHeuristics.hotnessWeights[i])
+                    elif matchResult == "D":
+                        hotness = hotness + (FormHeuristics.resultWeights[1] * FormHeuristics.hotnessWeights[i])
+                # print "Hotness of " + team + " is " + str( hotness/100)
+                return hotness/100                      
+        raise NameError("No matching form information found for " + team)
+
 # Example call
-# formHeuristics = FormHeuristics()
-# formDiscrepancyData = formHeuristics.getFormTableDiscrepancy()
-# for formDiscrepancy in formDiscrepancyData:
-#     print str(formDiscrepancy[0])
-#     print str(formDiscrepancy[1])
+formHeuristics = FormHeuristics()
+formDiscrepancyData = formHeuristics.getFormTableDiscrepancy()
+hotnessDiscrepancyData = formHeuristics.getHotnessDiscrepancy()
+print "----Form Discrepancy----"
+for formDiscrepancy in formDiscrepancyData:
+    print str(formDiscrepancy[0])
+    print str(formDiscrepancy[1])
+print "----Hotness Discrepancy----"
+for hotnessDiscrepancy in hotnessDiscrepancyData:
+    print str(hotnessDiscrepancy[0])
+    print str(hotnessDiscrepancy[1])
+     
